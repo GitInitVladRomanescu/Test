83c8b35b0e75e792d0cb782fb36f864880d7a47b
Vlad <vlad.romanescu@hotmail.com>
2021-08-30 00:10:31 +0300

    Crack the Password

---
+++ CrackPass.cs
@@ -0,0 +1,23 @@
+include iostream
+
+
+string password = vlad; 
+int counter = 0;
+int counter2 = 0;
+string chars = "abcdefghijklmnoprqstxyz";
+
+class CrackPass
+{
+    while (lenght(password) <= counter)
+    {
+        if password[counter] ==  chars[counter2]
+        {
+            counter++;
+        }
+        else
+        {
+            counter2++;
+        }
+    }
+}
+
